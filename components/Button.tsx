ca56564e7256e078a2b25955dd9102e173da5f7a
Vlad1sl4v <122152653+h4ze-07@users.noreply.github.com>
2024-03-28 14:57:01 +0200

    feat: added all sections + footer

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,11 +5,12 @@ type ButtonProps = {
     type: 'button' | 'submit',
     text: string,
     icon?: string,
-    variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text',
+    variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text' | 'btn_white' | 'btn_dark_green_outline',
+    full?: boolean,
 }
 
 
-const Button = ({type, text, icon, variant}: ButtonProps) => {
+const Button = ({type, text, icon, variant, full}: ButtonProps) => {
   return (
     <button type={type}
     className={`flexCenter gap-3 rounded-full border ${variant}`}
--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,3 +1,4 @@
+import { PEOPLE_URL } from "@/constants"
 import Image from "next/image"
 
 type CampCardProps = {
@@ -13,12 +14,28 @@ const CampCard = ({bgImage, title, subtitle, peopleJoined}: CampCardProps) => {
         <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
           <div className="flexCenter gap-4">
             <div className="rounded-full bg-green-50 p-4">
-              <Image src={'/folded-map.svg'} alt="folded map" width={28} height={28} />
+              <Image src={'/folded-map.svg'} alt="folded map" width={28} height={28} title="Map" />
             </div>
             <div className="flex flex-col gap-1">
               <h4 className="bold-18 text-white">{title}</h4>
               <p className="regular-14 text-white">{subtitle}</p>
             </div>
+          </div>
+
+          <div className="flexCenter gap-6">
+            <span className="flex -space-x-4 overflow-hidden">
+              {PEOPLE_URL.map((url) => (
+                <Image
+                  className="inline-block h-10 w-10 rounded-full" 
+                  src={url}
+                  key={url}
+                  alt="person"
+                  width={52}
+                  height={52}
+                />
+              ))}
+            </span>
+            <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
           </div>
         </div>
     </div>
@@ -35,7 +52,28 @@ const Camp = () => {
           subtitle='Prigen, Pasuruan'
           peopleJoined='50+ Joined'
         />
-        <CampCard />
+        <CampCard
+          bgImage='bg-bg-img-2'
+          title='Mountain View Camp'
+          subtitle='Somewhere in Beauty'
+          peopleJoined='30+ Joined'          
+        />
+      </div>
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"><strong>Feeling Lost</strong> And Not Knowing The Way?</h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Startfrom the anxiety of the climbers when visiting a new climbing location? the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure!
+          </p>
+          <Image
+              src={'/quote.svg'}
+              alt="quote"
+              width={186}
+              height={219}
+              className="camp-quote"
+            />
+        </div>
       </div>
     </section>
   )
